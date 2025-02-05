10773a2c3360d256a57fe2aeeaba680303b7e576
makzs <erikmakiak@gmail.com>
2025-02-04 23:38:37 -0300

    feat: Adding Culture Middleware and Exception Filter

--- src/Backend/MyRecipeBook.API/Controllers/UserController.cs
+++ src/Backend/MyRecipeBook.API/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Application.UseCases.User.Register;
 using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Communication.Responses;
 
@@ -13,7 +14,12 @@ namespace MyRecipeBook.API.Controllers
         [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
         public IActionResult Register(RequestRegisterUserJson request)
         {
-            return Created();
+
+            var useCase = new RegisterUserUseCase();
+
+            var result = useCase.Execute(request);
+
+            return Created(string.Empty, result);
         }
 
 
---
+++ src/Shared/MyRecipeBook.Exceptions/ExceptionsBase/ErrorOnValidationException.cs
@@ -0,0 +1,11 @@
+﻿namespace MyRecipeBook.Exceptions.ExceptionsBase;
+
+public class ErrorOnValidationException : MyRecipeBookException
+{
+    public IList<string> ErrorsMessages { get; set; }
+
+    public ErrorOnValidationException(IList<string> erros)
+    {
+        ErrorsMessages = erros;
+    }
+}
