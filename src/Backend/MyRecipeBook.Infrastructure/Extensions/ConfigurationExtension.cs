cdbcad1f2fbdd77eb17e47c71ff1370cff08e40b
makzs <erikmakiak@gmail.com>
2025-02-11 14:33:09 -0300

    feat: adding migration to create database "meulivrodereceitas"

---
+++ src/Backend/MyRecipeBook.Infrastructure/Extensions/ConfigurationExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyRecipeBook.Infrastructure.Extensions;
+
+public static class ConfigurationExtension
+{
+    public static string ConnectionString(this IConfiguration configuration)
+    {
+        return configuration.GetConnectionString("connection")!;
+    }
+}
