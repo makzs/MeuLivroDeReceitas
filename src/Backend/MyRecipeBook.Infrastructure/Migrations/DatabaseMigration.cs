b33b564a1e8d61de4ba410ba5ab1c93da5325c57
makzs <erikmakiak@gmail.com>
2025-02-11 16:19:51 -0300

    feat: adding migrations to create the table "users"

--- src/Backend/MyRecipeBook.Infrastructure/Migrations/DatabaseMigration.cs
+++ src/Backend/MyRecipeBook.Infrastructure/Migrations/DatabaseMigration.cs
@@ -1,13 +1,16 @@
 ﻿using Dapper;
+using FluentMigrator.Runner;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MyRecipeBook.Infrastructure.Migrations;
 
 public static class DatabaseMigration
 {
-    public static void Migrate(string connectionString)
+    public static void Migrate(string connectionString, IServiceProvider serviceProvider)
     {
         EnsureDatabaseCreated(connectionString);
+        MigrationDatabase(serviceProvider);
     }
 
     private static void EnsureDatabaseCreated(string connectionString)
@@ -27,5 +30,14 @@ public static class DatabaseMigration
 
         if (records.Any() == false)
             dbConnection.Execute($"CREATE DATABASE {databaseName}");
+    }
+
+    private static void MigrationDatabase(IServiceProvider serviceProvider)
+    {
+        var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+        runner.ListMigrations();
+
+        runner.MigrateUp();
     }
 }
