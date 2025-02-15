7159ae67c2ecebf5f8e9e0b5373dbb443d035b8e
makzs <erikmakiak@gmail.com>
2025-02-14 23:25:13 -0300

    feat: adding unit tests for userValidator

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -10,8 +10,10 @@ public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
     {
         RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessageException.NAME_EMPTY);
         RuleFor(user => user.Email).NotEmpty();
-        RuleFor(user => user.Email).EmailAddress();
         RuleFor(user => user.Password.Length).GreaterThanOrEqualTo(6);
-
+        When(user => string.IsNullOrEmpty(user.Email) == false, () => 
+        {
+            RuleFor(user => user.Email).EmailAddress();
+        });
     }
 }
