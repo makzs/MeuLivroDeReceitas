02dd2b614bbce993b0c77334418409c2962c84c2
makzs <erikmakiak@gmail.com>
2025-03-05 17:03:39 -0300

    feat: adding unit test to Login user case

---
+++ tests/UseCases.Test/Login/DoLogin/DoLoginUseCaseTest.cs
@@ -0,0 +1,62 @@
+﻿using CommonTestsUtilities.Cryptography;
+using CommonTestsUtilities.Entities;
+using CommonTestsUtilities.Repositories;
+using CommonTestsUtilities.Requests;
+using MyRecipeBook.Application.UseCases.Login.DoLogin;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+using Shouldly;
+using System.ComponentModel.DataAnnotations;
+
+namespace UseCases.Test.Login.DoLogin;
+
+public class DoLoginUseCaseTest
+{
+
+    [Fact]
+    public async Task Success()
+    {
+        (var user, var password) = UserBuilder.Build();
+
+        var useCase = CreateUserCase(user);
+
+        var result = await useCase.Execute(new RequestLoginJson
+        {
+            Email = user.Email,
+            Password = password
+
+        });
+
+        result.ShouldNotBeNull();
+        result.Name.ShouldNotBeNullOrWhiteSpace();
+        result.Name.ShouldBe(user.Name);
+
+    }
+
+    [Fact]
+    public async Task Error_Invalid_User()
+    {
+        var request = RequestLoginJsonBuilder.Build();
+
+        var useCase = CreateUserCase();
+
+        var result = useCase.Execute(request);
+
+        var exception = await Should.ThrowAsync<InvalidLoginException>(() => result);
+
+        exception.Message.ShouldBe(ResourceMessageException.EMAIL_OR_PASSWORD_INVALID);
+
+    }
+
+    private static DoLoginUseCase CreateUserCase(MyRecipeBook.Domain.Entities.User? user = null)
+    {
+        var passwordEncripter = PasswordEncripterBuilder.Build();
+        var userReadOnlyRepositoryBuilder = new UserReadOnlyRepositoryBuilder();
+
+        if (user is not null)
+            userReadOnlyRepositoryBuilder.GetByEmailAndPassword(user);
+
+        return new DoLoginUseCase(userReadOnlyRepositoryBuilder.Build(), passwordEncripter);
+    }
+}
